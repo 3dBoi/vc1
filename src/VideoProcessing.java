bcdaff8d129a70312bb50a396e04851faa41e5bd
3Dboi <77V^smqWrdMjUEmM>
2021-12-03 12:24:54 +0100

    no more laag

--- src/VideoProcessing.java
+++ src/VideoProcessing.java
@@ -13,43 +13,56 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.File;
+import java.util.ArrayList;
+
+
+import org.opencv.core.Mat;
 import org.opencv.core.Point;
+import org.opencv.core.MatOfPoint;
+
+import java.util.List;
+import org.opencv.core.Core;
+
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 
 /**
  * Simple application, that opens a video stream
  * - either from the web cam or a video file -
- * grabs frames, 
+ * grabs frames,
  * performs frame-wise image processing using OpenCV,
- * displays the video stream and the processed stream 
+ * displays the video stream and the processed stream
  * and saves processed frames to an image file.
- * 
+ *
  * @author Karsten Lehn
  * @version 4.9.2020
- * 
+ *
  */
 public class VideoProcessing extends JFrame {
 
 	private JButton confirmButton;
-    private BufferedImagePanel imgPanel1;
-    private BufferedImagePanel imgPanel2;
-    private static JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));
+	private BufferedImagePanel imgPanel1;
+	private BufferedImagePanel imgPanel2;
+	private static JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));
 	static int currentSigma;
 	//Hier werden die Hues gesaved für die detection!
 	private int firstHue;
 	private int secondHue;
+
+	List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 	/**
 	 * Create object and perform the processing by calling private member functions.
 	 */
-	
+
 	public VideoProcessing() {
 		imgPanel1 = null;
 		imgPanel2 = null;
-		
+
 		createFrame();
 		processShowVideo();
 	}
 
-	
+
 	/**
 	 * Returns the path and file of the video to be processed.
 	 * @return path and file name in one string
@@ -61,36 +74,36 @@ public class VideoProcessing extends JFrame {
 		filePathName = filePathName.substring(1);  // remove the bug
 */		// End: Get file path and name from "getRessource"
 
-		
-		// Begin: Set relative path and file name 
+
+		// Begin: Set relative path and file name
 //		String filePathName = "videos\\Landscape.avi";
-		// End: Set relative path and file name 
-		
+		// End: Set relative path and file name
+
 		// Choose file path and file name via a file selector box
-        int returnVal = fileChooser.showOpenDialog(this); 
-        if(returnVal != JFileChooser.APPROVE_OPTION) {
-            return null;  // cancelled
-        }
-        File selectedFile = fileChooser.getSelectedFile();
-        String filePathName = selectedFile.getPath();		
+		int returnVal = fileChooser.showOpenDialog(this);
+		if(returnVal != JFileChooser.APPROVE_OPTION) {
+			return null;  // cancelled
+		}
+		File selectedFile = fileChooser.getSelectedFile();
+		String filePathName = selectedFile.getPath();
 		// End: Choose file path and file name via a file selector box
-	    
-	    System.out.println("Video file name: " + filePathName);
+
+		System.out.println("Video file name: " + filePathName);
 		return filePathName;
 	}
-	
+
 	/**
 	 * @param imgMat image matrix to be written to a file
 	 * @param filename name of the file to be created
 	 */
 	private void writeImage(Mat imgMat, String filename) {
 		String filePathName = "videos/" + filename;
-	    Imgcodecs.imwrite(filePathName, imgMat, 
-	    		new MatOfInt(Imgcodecs.IMWRITE_PNG_STRATEGY_HUFFMAN_ONLY,
-	    					 Imgcodecs.IMWRITE_PNG_STRATEGY_FIXED));
+		Imgcodecs.imwrite(filePathName, imgMat,
+				new MatOfInt(Imgcodecs.IMWRITE_PNG_STRATEGY_HUFFMAN_ONLY,
+						Imgcodecs.IMWRITE_PNG_STRATEGY_FIXED));
 	}
-	
-	
+
+
 	/**
 	 * Create the JFrame to be displayed, displaying two images.
 	 */
@@ -99,7 +112,7 @@ public class VideoProcessing extends JFrame {
 		setTitle("Original and processed video stream");
 		JPanel contentPane = (JPanel) getContentPane();
 		contentPane.setLayout(new FlowLayout());
-		
+
 		imgPanel1 = new BufferedImagePanel();
 		contentPane.add(imgPanel1);
 		imgPanel2 = new BufferedImagePanel();
@@ -107,7 +120,7 @@ public class VideoProcessing extends JFrame {
 		contentPane.add(initSlider(contentPane));
 		initButton();
 		contentPane.add(confirmButton);
-	       // place the frame at the center of the screen and show
+		// place the frame at the center of the screen and show
 		pack();
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation(dim.width/2 - getWidth()/2, dim.height/2 - getHeight()/2);
@@ -120,32 +133,34 @@ public class VideoProcessing extends JFrame {
 	 * Reades a video stream from a file or camera, displays the original frames,
 	 * processes the frames and displays the result.
 	 */
-	private void processShowVideo() {	    
+	private void processShowVideo() {
 
 		// BEGIN: Prepare streaming from internal web cam
 		//NOTE: "To open default camera using default backend just pass 0."
 		//https://docs.opencv.org/3.4/d8/dfe/classcv_1_1VideoCapture.html#a57c0e81e83e60f36c83027dc2a188e80
- 		VideoCapture cap = new VideoCapture(0, Videoio.CAP_ANY);
-		// END: Prepare streaming from internal web cam		
+		VideoCapture cap = new VideoCapture(0, Videoio.CAP_ANY);
+		// END: Prepare streaming from internal web cam
 
-	   // BEGIN: Prepare streaming from video file
-       //String filePathName = getFilePathName();
-	   //VideoCapture cap = new VideoCapture(filePathName);
- 	   // END: Prepare streaming from video file
+		// BEGIN: Prepare streaming from video file
+		//String filePathName = getFilePathName();
+		//VideoCapture cap = new VideoCapture(filePathName);
+		// END: Prepare streaming from video file
 
-       Mat frame = new Mat();
-	   // Check of file or camera can be opened
-	   if(!cap.isOpened()) 
-		   throw new CvException("The Video File or the Camera could not be opened!");
-        
-	   Mat processedImage = new Mat();  
-	   cap.read(frame);
-	   System.out.println("  First grabbed frame: " + frame);
-	   System.out.println("  Matrix columns: " + frame.cols());
-	   System.out.println("  Matrix rows: " + frame.rows());
-	   System.out.println("  Matrix channels: " + frame.channels());
+		Mat frame = new Mat();
+		Mat grey = new Mat();
+		Mat circles = new Mat();
+		// Check of file or camera can be opened
+		if(!cap.isOpened())
+			throw new CvException("The Video File or the Camera could not be opened!");
 
-	   //HSV threshold selection
+		Mat processedImage = new Mat();
+		cap.read(frame);
+		System.out.println("  First grabbed frame: " + frame);
+		System.out.println("  Matrix columns: " + frame.cols());
+		System.out.println("  Matrix rows: " + frame.rows());
+		System.out.println("  Matrix channels: " + frame.channels());
+
+		//HSV threshold selection
 //	   int i = 1;
 //	   int hLow = 100; //Max 180
 //	   int sLow = 100; //Max 255
@@ -156,93 +171,109 @@ public class VideoProcessing extends JFrame {
 
 
 		int i = 1;
-	   int hLow = 0; //Max 180
-	   int sLow = 40; //Max 255
-	   int vLow = 20; //Max 255
-	   int hHigh = 180; //Max 180
-	   int sHigh = 255; //Max 255
-	   int vHigh = 255; //Max 255
+		int hLow = 0; //Max 180
+		int sLow = 40; //Max 255
+		int vLow = 20; //Max 255
+		int hHigh = 180; //Max 180
+		int sHigh = 255; //Max 255
+		int vHigh = 255; //Max 255
 
-	   System.out.print("Frame count: (" + i + ")"); 
-	   // loop for grabbing frames
-	   while (cap.read(frame)) {
+		System.out.print("Frame count: (" + i + ")");
+		// loop for grabbing frames
+		while (cap.read(frame)) {
 
-		   // Gaussian Blur for smoother detection
-		   Imgproc.GaussianBlur(frame, frame, new Size(11, 11), 4);
+			// Gaussian Blur for smoother detection
+			Imgproc.GaussianBlur(frame, frame, new Size(11, 11), 4);
+			Imgproc.cvtColor(frame, grey, Imgproc.COLOR_BGR2GRAY);
 
-    	   // convert the frame to HSV, output processedImage
-    	   Imgproc.cvtColor(frame, processedImage, Imgproc.COLOR_BGR2HSV);
+			// convert the frame to HSV, output processedImage
+			Imgproc.cvtColor(frame, processedImage, Imgproc.COLOR_BGR2HSV);
 
-		   // apply HSV threshold and output binary image
-		   //Core.inRange(processedImage, new Scalar(hLow,sLow,vLow), new Scalar(hHigh,sHigh,vHigh), processedImage);
-		   Core.inRange(processedImage, new Scalar(getCurrentSigma(),sLow,vLow), new Scalar(getCurrentSigma()+15,sHigh,vHigh), processedImage);
+			// apply HSV threshold and output binary image
+			//Core.inRange(processedImage, new Scalar(hLow,sLow,vLow), new Scalar(hHigh,sHigh,vHigh), processedImage);
+			Core.inRange(processedImage, new Scalar(getCurrentSigma(),sLow,vLow), new Scalar(getCurrentSigma()+15,sHigh,vHigh), processedImage);
 
-		   // apply medianBlur for noise reduction
-    	   Imgproc.medianBlur(processedImage, processedImage, 15);
+			// apply medianBlur for noise reduction
+			Imgproc.medianBlur(processedImage, processedImage, 15);
 
-		   //Circle detection
-		   Imgproc.HoughCircles(processedImage, processedImage, Imgproc.HOUGH_GRADIENT, 1,  (double)processedImage.rows()/16,
-				   100.0, 30.0, 1, 30);
+			//Circle detection
+//			Imgproc.HoughCircles(processedImage, circles, Imgproc.HOUGH_GRADIENT, 1.0,
+//					(double)grey.rows()/16, // change this value to detect circles with different distances to each other
+//					100.0, 30.0, 0, 0); // change the last two parameters
+//			// (min_radius & max_radius) to detect larger circles
+//			for (int x = 0; x < circles.cols(); x++) {
+//				double[] c = circles.get(0, x);
+//				Point center = new Point(Math.round(c[0]), Math.round(c[1]));
+//				// circle center
+//				Imgproc.circle(processedImage, center, 1, new Scalar(0,100,100), 3, 8, 0 );
+//				// circle outline
+//				int radius = (int) Math.round(c[2]);
+//				Imgproc.circle(processedImage, center, radius, new Scalar(255,0,255), 3, 8, 0 );
+//			}
 
-		   System.out.println(processedImage.cols());
+			//Find Contours
+			Imgproc.findContours(processedImage, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_NONE);
+			double maxArea = 100;
+			float[] radius = new float[1];
+			Point center = new Point();
+			for (MatOfPoint c : contours) {
+				if (Imgproc.contourArea(c) > maxArea) {
+					MatOfPoint2f c2f = new MatOfPoint2f(c.toArray());
+					Imgproc.minEnclosingCircle(c2f, center, radius);
+				}
+			}
 
+			//Imgproc.drawContours(frame,contours,5,new Scalar(255,0,0),5);
 
-		   //Circle einzeichnen
-		   for (int x = 0; x < processedImage.cols()+1; x++) {
-			   double[] c = processedImage.get(0, x);
-			   Point center = new Point(Math.round(c[0]), Math.round(c[1]));
-			   // circle center
-			   Imgproc.circle(processedImage, center, 1, new Scalar(0,100,100), 3, 8, 0 );
-			   // circle outline
-			   int radius = (int) Math.round(c[2]);
-			   Imgproc.circle(processedImage, center, radius, new Scalar(255,0,255), 3, 8, 0 );
-		   }
+			Imgproc.circle(frame, center, (int)radius[0], new Scalar(255, 0, 0), 2);
 
+			// display original frame from the video stream
+			imgPanel1.setImage(Mat2BufferedImage(frame));
 
-		   // display original frame from the video stream
-		   imgPanel1.setImage(Mat2BufferedImage(frame));
+			// Show processed image
+			imgPanel2.setImage(Mat2BufferedImage(processedImage));
+			pack();
 
-    	   // Show processed image
-    	   imgPanel2.setImage(Mat2BufferedImage(processedImage));
-    	   pack();
-        
-		 //Write unprocessed and processed frame successively to files
-		   // writeImage(frame, "unprocessedImage.png");
-		   // writeImage(processedImage, "processedImage.png");
-	   } // END for loop
-	   System.out.println(".(" + i + ")");
-	   cap.release();
+			contours.clear();
+
+			
+			//Write unprocessed and processed frame successively to files
+			// writeImage(frame, "unprocessedImage.png");
+			// writeImage(processedImage, "processedImage.png");
+		} // END for loop
+		System.out.println(".(" + i + ")");
+		cap.release();
 	}
-    
+
 	/**
 	 * Converts an OpenCV matrix into a BufferedImage.
-	 * 
-	 * Inspired by 
- 	 * http://answers.opencv.org/question/10344/opencv-java-load-image-to-gui/
- 	 * Fastest code
-	 * 
+	 *
+	 * Inspired by
+	 * http://answers.opencv.org/question/10344/opencv-java-load-image-to-gui/
+	 * Fastest code
+	 *
 	 * //@param OpenCV Matrix to be converted must be a one channel (grayscale) or
 	 * three channel (BGR) matrix, i.e. one or three byte(s) per pixel.
 	 * @return converted image as BufferedImage
-	 * 
+	 *
 	 */
-    public BufferedImage Mat2BufferedImage(Mat imgMat){
-    	int bufferedImageType = 0;
-    	switch (imgMat.channels()) {
-		case 1:
-			bufferedImageType = BufferedImage.TYPE_BYTE_GRAY;
-			break;
-		case 3:
-			bufferedImageType = BufferedImage.TYPE_3BYTE_BGR;
-			break;
-		default:
-    		throw new IllegalArgumentException("Unknown matrix type. Only one byte per pixel (one channel) or three bytes pre pixel (three channels) are allowed.");
+	public BufferedImage Mat2BufferedImage(Mat imgMat){
+		int bufferedImageType = 0;
+		switch (imgMat.channels()) {
+			case 1:
+				bufferedImageType = BufferedImage.TYPE_BYTE_GRAY;
+				break;
+			case 3:
+				bufferedImageType = BufferedImage.TYPE_3BYTE_BGR;
+				break;
+			default:
+				throw new IllegalArgumentException("Unknown matrix type. Only one byte per pixel (one channel) or three bytes pre pixel (three channels) are allowed.");
 		}
-    	BufferedImage bufferedImage = new BufferedImage(imgMat.cols(), imgMat.rows(), bufferedImageType);
-    	final byte[] bufferedImageBuffer = ((DataBufferByte) bufferedImage.getRaster().getDataBuffer()).getData();
-    	imgMat.get(0, 0, bufferedImageBuffer);
-    	return bufferedImage;
-    }
+		BufferedImage bufferedImage = new BufferedImage(imgMat.cols(), imgMat.rows(), bufferedImageType);
+		final byte[] bufferedImageBuffer = ((DataBufferByte) bufferedImage.getRaster().getDataBuffer()).getData();
+		imgMat.get(0, 0, bufferedImageBuffer);
+		return bufferedImage;
+	}
 
 	public void initButton(){
 
